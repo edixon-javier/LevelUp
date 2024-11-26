29b5a5c121c14fa91c33582256f0707da3315744
edixon-Lizcano <abatar.111@gmail.com>
2024-11-25 22:30:58 -0500

    remove the unnecessary component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
-import { CarouselApi } from "./CarouselApi";
+import React from "react";
 import { ShoppingCart } from "./ShoppingCart";
-import { TodoApp } from "./TodoApp";
 
 function App() {
-  const [activeC, setActiveC] = useState("ShoppingCart");
-  const [showHeader, setShowHeader] = useState(true);
-
-  const components = {
-    TodoApp: <TodoApp />,
-    CarouselApi: <CarouselApi />,
-    ShoppingCart: <ShoppingCart />,
-  };
-
-  const toggleHeader = () => {
-    setShowHeader(!showHeader);
-  };
-
-  return (
-    <div>
-      <header className={showHeader ? "header-hidden" : ""}>
-        <button onClick={() => {setActiveC("TodoApp"); toggleHeader();}}>
-          Todo List
-        </button>
-        <button onClick={() => {setActiveC("CarouselApi"); toggleHeader();}}>Carousel</button>
-        <button onClick={() => {setActiveC("ShoppingCart"); toggleHeader();}}>
-          Shopping Cart
-        </button>
-      </header>
-      <button className="especial-button" onClick={() => toggleHeader()}>
-        🪅
-      </button>
-      <div>{components[activeC]}</div>
-    </div>
-  );
+  return <ShoppingCart />;
 }
 
 export default App;
